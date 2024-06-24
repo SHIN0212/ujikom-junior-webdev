5ba40271f6854310cd9300bfe50cd78083ad3539
Rafli Nur <asus_522d@outlook.com>
2024-06-24 14:25:39 +0700

    [FEAT] Datatable

--- resources/views/layouts/dashboard.blade.php
+++ resources/views/layouts/dashboard.blade.php
@@ -24,7 +24,6 @@
 </head>
 
 <body class="g-sidenav-show   bg-gray-100">
-    <div class="min-height-300 bg-primary position-absolute w-100"></div>
    @include('layouts.partials.sidebar')
     <main class="main-content position-relative border-radius-lg ">
         <div class="container-fluid py-4">
